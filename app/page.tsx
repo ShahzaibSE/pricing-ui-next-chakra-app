dd1f3f025e239ad6c5bf817b870b9e7a664115f5
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-01-19 22:58:16 +0500

    FEAT: Designing header section of pricing UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
+"use client";
+
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
-import { Grid, GridItem } from "@chakra-ui/react";
+import {
+  Grid,
+  GridItem,
+  Flex,
+  Spacer,
+  SimpleGrid,
+  Box,
+  Center,
+} from "@chakra-ui/react";
+// Components.
+import Header from "../components/Header";
 
 // const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+        <Header />
+  );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+export default function Header() {
+  return (
+    <Box bg="#6B46C1" color="#F7FAFC" pt="90px" pb="198px">
+      <Heading
+        textAlign="center"
+        fontWeight="800"
+        fontSize="48px"
+        color="#F7FAFC"
+      >
+        Simple pricing for your business
+      </Heading>
+      <Text textAlign="center" fontWeight="500" fontSize="24px" color="#F7FAFC">
+        Plans that are carefully crafted to suit your business.
+      </Text>
+    </Box>
+  );
+}
